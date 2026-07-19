109fc4372ec81736ef4a7c3e1b9f3e40526fd826
richardsnapes9-dot <richardsnapes9@gmail.com>
2026-07-19 13:56:42 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+const botao = document.getElementById("entrar");
+const musica = document.getElementById("musica");
 
+botao.addEventListener("click", () => {
+
+    musica.play();
+
+    document.getElementById("inicio").style.display = "none";
+
+    alert("Nossa história começa agora... ❤️");
+
+});
